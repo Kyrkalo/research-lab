71a56bb961366c6daed414a439fad8652fc57d81
Roman Kyrkalo <kyrkalo.roman@live.com>
2025-06-14 21:47:19 -0400

    chapter 4

---
+++ chapter4.py
@@ -0,0 +1,89 @@
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
+
+import torch
+import torch.nn as nn
+import tiktoken
+
+from models.dummyGPTModel import DummyGPTModel
+from models.feedForward import ExampleDeepNeuralNetwork, FeedForward, print_gradients
+from models.layerNorm import LayerNorm
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+batch = []
+
+txt1 = "Every effort moves you"
+txt2 = "Every day holds a"
+
+batch.append(torch.tensor(tokenizer.encode(txt1)))
+batch.append(torch.tensor(tokenizer.encode(txt2)))
+batch = torch.stack(batch, dim=0)
+print(batch)
+
+torch.manual_seed(123)
+model = DummyGPTModel(GPT_CONFIG_124M)
+
+logits = model(batch)
+print("Output shape:", logits.shape)
+print(logits)
+
+# 4.2 Normalizing activations with layer normalization
+
+torch.manual_seed(123)
+
+# create 2 training examples with 5 dimensions (features) each
+batch_example = torch.randn(2, 5) 
+print(batch_example)
+layer = nn.Sequential(nn.Linear(5, 6), nn.ReLU())
+out = layer(batch_example)
+print(out)
+
+mean = out.mean(dim=-1, keepdim=True)
+var = out.var(dim=-1, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+
+mean = out_ln.mean(dim=-1, keepdim=True)
+var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
+
+print("Mean:\n", mean)
+print("Variance:\n", var)
+
+#4.3 Implementing a feed forward network with GELU activations
+
+
+print(GPT_CONFIG_124M["emb_dim"])
+
+ffn = FeedForward(GPT_CONFIG_124M)
+
+# input shape: [batch_size, num_token, emb_size]
+x = torch.rand(2, 3, 768) 
+out = ffn(x)
+print(out.shape)
+
+# 4.4 Adding shortcut connections
+
+layer_sizes = [3, 3, 3, 3, 3, 1]  
+
+sample_input = torch.tensor([[1., 0., -1.]])
+
+torch.manual_seed(123)
+model_without_shortcut = ExampleDeepNeuralNetwork(
+    layer_sizes, use_shortcut=False
+)
+print_gradients(model_without_shortcut, sample_input)
+
+# 4.5 Connecting attention and linear layers in a transformer block
+
