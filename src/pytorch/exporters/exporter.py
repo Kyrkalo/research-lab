3d2e96093353d7cc3e31358fea7d47324e081a26
Roman Kyrkalo <kyrkalo.roman@live.com>
2025-09-21 23:01:54 -0400

    #81 Add base exporter

---
+++ src/pytorch/exporters/exporter.py
@@ -0,0 +1,17 @@
+
+from anyio import Path
+
+
+class Exporter():
+
+    def __init__(self, config=None):
+        self.config = config if config else {}
+
+    def getPath(self, extension, prefix=""):
+        return Path(f"{prefix}{self.dcgan_config["model_name"]}.{extension}")
+
+    def run(self):
+        return self
+    
+    def setup(self):
+        return self
